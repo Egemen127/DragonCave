34f53d3c9b2483f7904276f3eccc03edc41c212b
Egemen <egemendurmuss@gmail.com>
2022-04-05 19:39:08 -0400

    Refactored the code and added a unit test

---
+++ DragonCaveTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DragonCaveTest {
+
+    @Test
+    void displayResultTests() {
+
+            assertAll("All cases",
+                    ()->assertEquals("Gobbles you down in one byte!", DragonCave.displayResult("1")),
+                    ()->assertEquals("Offers you some of his treasure. You got lucky this time.", DragonCave.displayResult("2"))
+            );
+
+
+    }
+}
